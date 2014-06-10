a77d9395dac8ff221c003140f55f2b7f43c764df
Boris <bschapira@clever-age.com>
2014-06-10 14:09:38 +0200

    Output of XML declaration via an echo statement, in case your code is ever used on a server that is (poorly) configured to use short open tags.

--- index.php
+++ index.php
@@ -65,8 +65,8 @@ if ( ! isset ( $_REQUEST ['charspacing'] ) ) {
     $_REQUEST['charspacing'] = '100';
 }
 
-
-?><?xml version="1.0" ?>
+echo '<?xml version="1.0" ?'.'>'
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
