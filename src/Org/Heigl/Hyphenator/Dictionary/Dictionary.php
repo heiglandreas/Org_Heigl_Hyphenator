83f4ebdb4a77d8ba645f55cc87543d6068cb0b67
Andreas Heigl <andreas@heigl.org>
2015-11-30 22:50:11 +0100

    Fixes Multibyte-issues
    Thsi commit finally adds a russion hyphenation pattern

--- src/Org/Heigl/Hyphenator/Dictionary/Dictionary.php
+++ src/Org/Heigl/Hyphenator/Dictionary/Dictionary.php
@@ -154,13 +154,13 @@ class Dictionary
                 continue;
             }
             // Remove all Upper-case items as they are OOo-specific
-            if ( $item === strtoupper($item) ) {
+            if ($item === mb_strtoupper($item) ) {
                 continue;
             }
             // Ignore lines containing an '=' sign as these are specific
             // instructions for non-standard-hyphenations. These will be
             // implemented later.
-            if ( false !== strpos($item, '=') ) {
+            if ( false !== mb_strpos($item, '=') ) {
                 continue;
             }
             $item = mb_convert_Encoding($item, 'UTF-8', $source);
