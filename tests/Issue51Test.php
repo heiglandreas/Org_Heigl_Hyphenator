e54c0c2c3e6e6c10b4135f2bc2df4e64dfc283fc
Andreas Heigl <andreas@heigl.org>
2022-05-21 09:59:14 +0200

    Update PHP-Versions

---
+++ tests/Issue51Test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Copyright Andreas Heigl <andreas@heigl.org>
+ *
+ * Licenses under the MIT-license. For details see the included file LICENSE.md
+ */
+
+namespace Org\Heigl\HyphenatorTest;
+
+use PHPUnit\Framework\TestCase;
+
+class Issue51Test extends TestCase
+{
+
+}
